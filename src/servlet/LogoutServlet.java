5dc9eff7fefd14f5c7008a2a059c73384904629b
mosesmpilusa <mosesmpilusa@gmail.com>
2026-05-12 19:04:23 +0200

    Create LogoutServlet to manage user logout
    Refactored AuthSB to LogoutServlet for handling logout functionality.

--- src/servlet/LogoutServlet.java
+++ src/servlet/LogoutServlet.java
@@ -1,27 +1,28 @@
 package servlet;
 
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
-import za.ac.tut.entity.User;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- *
  * @author moses
  */
-@Stateless
-public class AuthSB implements AuthSBLocal {
+public class LogoutServlet extends HttpServlet {
 
-    @EJB
-    private UserFacadeLocal ufl;
     @Override
-    public User authenticate(String email, String password) {
-    
-        User user = ufl.findByEmail(email); 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-        if (user != null && user.getPassword().equals(password)) {
-            return user;
+        HttpSession session = request.getSession(false); 
+
+        if (session != null) {
+            session.invalidate();
         }
-        return null;
-       
-}
+       String url ="login.jsp";
+        RequestDispatcher disp = request.getRequestDispatcher(url);
+    }
 }
