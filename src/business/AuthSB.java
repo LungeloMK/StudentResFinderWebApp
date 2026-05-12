2bd3c1e74866ab1967f773ced30d9632d0186d53
mosesmpilusa <mosesmpilusa@gmail.com>
2026-05-12 19:10:32 +0200

    Add AuthSB class for user authentication
    Implement authentication method using UserFacadeLocal.

--- src/business/AuthSB.java
+++ src/business/AuthSB.java
@@ -1,4 +1,27 @@
 package business;
 
-public class AuthSB {
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import za.ac.tut.entity.User;
+
+/**
+ *
+ * @author moses
+ */
+@Stateless
+public class AuthSB implements AuthSBLocal {
+
+    @EJB
+    private UserFacadeLocal ufl;
+    @Override
+    public User authenticate(String email, String password) {
+    
+        User user = ufl.findByEmail(email); 
+
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
+       
+}
 }
